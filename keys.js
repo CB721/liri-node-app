37a070fe5774f083c501371a85e2f5c4350f8a21
Clint Brodar <clintbrodar@Clints-MacBook-Pro.local>
2019-06-02 09:08:01 -0700

    Getting response back from Spotify

--- keys.js
+++ keys.js
@@ -1,16 +1,6 @@
-console.log('this is loaded');
+console.log('the keys are loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-exports.omdb = {
-  id: process.env.OMBD_ID,
-  secret: process.env.OMBD_SECRET
-};
-
-exports.bandsInTown = {
-  id: process.env.BANDSINTOWN_ID,
-  secret: process.env.BANDSINTOWN_SECRET
-}
